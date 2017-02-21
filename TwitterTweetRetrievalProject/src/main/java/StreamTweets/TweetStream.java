d8a6c061c9fcbc29722b6aefef134b29749eee64
mplummer123 <mplummer3011@gmail.com>
2017-02-21 12:46:58 +0000

    added in file creating and writing. Over rights file with each iteration so this needs to be fixed

--- TwitterTweetRetrievalProject/src/main/java/StreamTweets/TweetStream.java
+++ TwitterTweetRetrievalProject/src/main/java/StreamTweets/TweetStream.java
@@ -7,17 +7,33 @@ package StreamTweets;
 import twitter4j.*;
 
 import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
 
 public abstract class TweetStream implements StatusListener {
 
     public static void main(String[] args) throws TwitterException, IOException{
+
+        final Path file = Paths.get("TwitterStreamForTrump.txt");
 
         TwitterStream twitterStream = new TwitterStreamFactory().getInstance();
 
         StatusListener listener = new StatusListener(){
 
             public void onStatus(Status status) {
-                System.out.println(status.getCreatedAt() + "," + status.getText() + "\n");
+                try {
+                    System.out.println(status.getCreatedAt() + "," + status.getText() + "\n");
+                    List<String> lines = Arrays.asList(status.getCreatedAt() + "," + status.getText() + "\n");
+                    //Creates and writes to file in the working directory i.e. the location of the project.
+                    Files.write(file, lines, Charset.forName("UTF-8"));
+                }
+                catch(IOException e){
+                    System.out.print(e);
+                }
             }
 
             public void onDeletionNotice(StatusDeletionNotice statusDeletionNotice) {}
@@ -40,8 +56,7 @@ public abstract class TweetStream implements StatusListener {
         };
 
         FilterQuery tweetFilterQuery = new FilterQuery();
-
-        String keywords[] = {"#TRUMP"};
+        String keywords[] = {"#Trump"};
         tweetFilterQuery.track(keywords);
         tweetFilterQuery.language(new String[]{"en"});
         twitterStream.addListener(listener);
