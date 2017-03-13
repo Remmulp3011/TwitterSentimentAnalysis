3f41349ed3b5c851aedad4b9fd771ba8d278f7ea
mplummer123 <mplummer3011@gmail.com>
2017-03-13 15:20:08 +0000

    Fixed issue with regards to collection not being updated. Cause and solution is commented in code Added counters for positive negative and neutral tweets Added comments and removed print lines.

--- TwitterTweetRetrievalProject/src/main/java/MongoDB/MatchSentiment.java
+++ TwitterTweetRetrievalProject/src/main/java/MongoDB/MatchSentiment.java
@@ -19,8 +19,12 @@ public class MatchSentiment {
         String wordPolarity;
         Boolean matchFound;
         String twitterText;
-        String tweetId;
+        String tweetId; //See notes to find out why this is not used. Being left in for further implementation.
         String sentimentWord;
+        int sentimentCounterPositive = 0;
+        int sentimentCounterNegative = 0;
+        int totalDocuments = 0;
+        int sentimentCounterNeutral = 0;
 
         //Connect to Database
         try {
@@ -58,6 +62,7 @@ public class MatchSentiment {
             DBCursor twitterDataCursor = twitterColl.find();
             //loading in Twitter table
             while (twitterDataCursor.hasNext()) {
+                totalDocuments += 1;
                 BasicDBObject currentDocument = (BasicDBObject) twitterDataCursor.next();
                 twitterDocumentList.add(currentDocument.getString("_id"));
                 twitterDocumentList.add(currentDocument.getString("tweetText"));
@@ -67,29 +72,33 @@ public class MatchSentiment {
 
                 overallSentimentValue = 0;//reset the sentiment value
                 tweetId = twitterDocumentList.get(0);
+                twitterText = twitterDocumentList.get(1);
 
                 for (int sentimentAndPolarityIndex = 0; sentimentAndPolarityIndex < sentimentWordDocumentList.size(); sentimentAndPolarityIndex++) {
-                    twitterText = twitterDocumentList.get(1);
                     sentimentWord = sentimentWordDocumentList.get(sentimentAndPolarityIndex);
                     matchFound = twitterText.contains(sentimentWord);
 
                     if (matchFound == true) {
 
-                        System.out.println("MATCH FOUND!!");//used for debugging
-
                         wordPolarity = sentimentPolarityDocumentList.get(sentimentAndPolarityIndex);
+
                         /*
-                        BasicDBObject newTwitterDataDocument = new BasicDBObject();
+                        Wont work due to the face that with each iteration when using $set it will be replaced.
+                        Therefore the value will not be added so a record will not be made it will be replaced each time.
+                        If $set is not used a new document will be created with the specified contents copied and the change made to the copy for the field.
+                        */
+
+/*                        BasicDBObject newTwitterDataDocument = new BasicDBObject();
 
                         //Key to change and value to change to. Use $set to only change specified key value
                         newTwitterDataDocument.append("$set", new BasicDBObject().append("sentimentFound", sentimentWordDocumentList.get(sentimentAndPolarityIndex) + ","));
 
                         tweetId = twitterDocumentList.get(0);
-                        //Find the id that needs to be changed
-                        BasicDBObject searchQuery = new BasicDBObject().append("_id", tweetId);
+
+                        //Find the tweet that needs to be changed, see explanation below as to why tweetText is used.
+                        BasicDBObject searchQuery = new BasicDBObject().append("tweetText", twitterText);
                         //Find the id and change the value to stated
-                        twitterColl.insert(searchQuery,newTwitterDataDocument);
-                        */
+                        twitterColl.insert(searchQuery,newTwitterDataDocument);*/
 
                         if (wordPolarity.equals("positive")) {
                             overallSentimentValue += 1;
@@ -98,17 +107,44 @@ public class MatchSentiment {
                         }
                     }
                 }
+                if(overallSentimentValue > 0)
+                {
+                    sentimentCounterPositive += 1;
+                }
+                else if (overallSentimentValue < 0)
+                {
+                    sentimentCounterNegative += 1;
+                }
+                else
+                {
+                    sentimentCounterNeutral += 1;
+                }
+
                 BasicDBObject newTwitterDataDocument = new BasicDBObject();
 
                 //Key to change and value to change to. Use $set to only change specified key value
                 newTwitterDataDocument.append("$set", new BasicDBObject().append("overallSentiment", overallSentimentValue));
-                //Find the id that needs to be changed
-                BasicDBObject searchQuery = new BasicDBObject().append("_id", tweetId);
+                //Construct the string that needs to be searched in order to find the correct id
+                /*
+                Having to do twitter text due to the face that quotations are added to the parameter.
+                e.g.for searching for ID you need: ObjectID("+tweetId+")
+                "ObjectID(\""+tweetId+"\")" when declared will become "ObjectID(\"+tweetId+\")" due to the addition quotations.
+                Therefore as it is not in the correct for with the addition quotes it will not be found.
+                Solution could be to change code to create my own ID system so a random object ID is not created.
+                For now the twitterText field will be used as it is unique for each tweet.
+                In the rare occasion someone will copy and paste a Tweet both will be the same and both will be updated each time they are met.
+                */
+                //Find the tweet that needs to be changed
+                BasicDBObject searchQuery = new BasicDBObject().append("tweetText", twitterText);
                 //Find the id and change the value to that stated
                 twitterColl.update(searchQuery, newTwitterDataDocument);
 
                 twitterDocumentList.clear();//clear the array list for the Twitter data for next iteration
             }
+            System.out.println("Number of tweets positive = " + sentimentCounterPositive);
+            System.out.println("Number of tweets negative = " + sentimentCounterNegative);
+            System.out.println("Number of tweets neutral = " + sentimentCounterNeutral);
+            System.out.println("Number of tweets = " + totalDocuments);
         } catch (IOException e) {
             System.out.println(e);
         }
